e7fbdb4e02d517c4e35d9cd3b2d675425b93d96e
ST10159832 <129107972+ST10159832@users.noreply.github.com>
2024-09-10 19:35:02 +0200

    Add files via upload
    this is my project with 6 window forms created but not completed

---
+++ Prog6212PartOne_ST10159832/Prog6212PartOne_ST10159832/LecturerDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prog6212PartOne_ST10159832
+{
+    /// <summary>
+    /// Interaction logic for LecturerDetails.xaml
+    /// </summary>
+    public partial class LecturerDetails : Window
+    {
+        public LecturerDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
